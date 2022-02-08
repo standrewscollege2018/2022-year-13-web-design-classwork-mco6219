2fe2461ccce37a28889d97b850f96ec9ce3ee06e
mco6219 <mco6219@stacmail.net>
2022-02-09 10:30:44 +1300

    Class work

---
+++ learn_bootstrap.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+<!--- container fluid goes all the way to the sides of the screen--->
+<div class="container-fluid">
+  <div class="row gy-2">
+    <div class="col-12 col-md-6 col-xl-2 bg-success">
+      <h1 class="display-3">Column 1</h1>
+    </div>
+
+    <div class="col-12 col-md-6 col-xl-2 bg-primary">
+      <h1 class="display-3">Column 2</h1>
+    </div>
+
+    <div class="col-12 col-md-6 col-xl-2 bg-warning">
+      <h1 class="display-3">Column 3</h1>
+    </div>
+
+    <div class="col-12 col-md-6 col-xl-2 bg-danger">
+      <h1 class="display-3">Column 4</h1>
+    </div>
+
+    <div class="col-12 col-md-6 col-xl-2 bg-info">
+      <h1 class="display-3">Column 5</h1>
+    </div>
+
+      <div class="col-12 col-md-6 col-xl-2 bg-secondary">
+        <h1 class="display-3">Column 6</h1>
+      </div>
+    </div>
+  </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
